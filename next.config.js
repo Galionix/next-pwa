f65b412223c49b492b8e5b7a0e873ab8750560b2
galionix <parfumeland@ya.ru>
2021-09-07 04:16:05 +0300

    init

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+// module.exports = {
+// 	reactStrictMode: true,
+// }
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+	reactStrictMode: true,
+	// other next config
+	pwa: {
+		dest: 'public',
+		// disable: process.env.NODE_ENV === 'development',
+		// register: true,
+		// scope: '/app',
+		// sw: 'service-worker.js',
+	},
+})
