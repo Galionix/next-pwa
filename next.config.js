1868f3d7fc51db0cf7db0437435bab93662b26f7
galionix <parfumeland@ya.ru>
2021-09-09 02:00:32 +0300

    upd

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = withPWA({
 	pwa: {
 		dest: 'public',
 		// disable: process.env.NODE_ENV === 'development',
-		// register: true,
+		register: true,
 		// scope: '/app',
 		// sw: 'service-worker.js',
 	},
