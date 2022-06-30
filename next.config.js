38e34afd0486014cea8076cfa6f53b50214f6717
galionix <parfumeland@ya.ru>
2022-06-30 13:24:29 +0300

    fix sidebar panel background color

--- next.config.js
+++ next.config.js
@@ -1,27 +1,11 @@
 /** @type {import('next').NextConfig} */
-// module.exports = {
-// 	reactStrictMode: true,
-// }
+
 const withPlugins = require('next-compose-plugins')
 
 const pwa = require('next-pwa')
 const nextTranslate = require('next-translate')
 
-// module.exports = withPWA({
-// 	reactStrictMode: true,
-// 	// other next config
-// 	i18n: {
-// 		locales: ['en', 'ru'],
-// 		defaultLocale: 'ru',
-// 	},
-// 	pwa: {
-// 		dest: 'public',
-// 		// disable: process.env.NODE_ENV === 'development',
-// 		register: true,
-// 		// scope: '/app',
-// 		// sw: 'service-worker.js',
-// 	},
-// })
+
 
 module.exports = withPlugins([
 	// add a plugin with specific configuration
